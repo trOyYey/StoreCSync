8fc7d1f10d338fe16292bc351d040d91d5345346
trOyYey <troyzuwara@gmail.com>
2024-09-05 23:47:25 +0000

    Mysql credintials

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS SCS_dev_db;
+CREATE USER IF NOT EXISTS 'SCS_dev'@'localhost' IDENTIFIED BY 'SCS_dev_pwd';
+GRANT ALL PRIVILEGES ON `SCS_dev_db`.* TO 'SCS_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'SCS_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS SCS_test_db;
+CREATE USER IF NOT EXISTS 'SCS_test'@'localhost' IDENTIFIED BY 'SCS_test_pwd';
+GRANT ALL PRIVILEGES ON `SCS_test_db`.* TO 'SCS_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'SCS_test'@'localhost';
 FLUSH PRIVILEGES;
